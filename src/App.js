dd74972c37833544f9c49af4adf8c39b6227d6f5
zhalice2011 <zhalice2012@gmail.com>
2018-08-27 12:46:06 +0800

    feat: add Draft-js and basic css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Editor, EditorState } from 'draft-js';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    }
+  }
+
+  onChange = (editorState) => {
+    this.setState({ editorState })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Editor
+        onChange={this.onChange} 
+        editorState = {this.state.editorState} 
+      /> 
+    )
   }
 }
 
