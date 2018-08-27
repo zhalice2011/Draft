590f066398f0ffe384d89ff713ea4c344c07ea20
zhalice2011 <zhalice2012@gmail.com>
2018-08-27 13:56:49 +0800

    feat: added rich text key commands

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import './App.css';
 
 class App extends Component {
@@ -13,12 +13,38 @@ class App extends Component {
   onChange = (editorState) => {
     this.setState({ editorState })
   }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+    if (newState) {
+      this.onChange(newState);
+      return 'handled' // 处理
+    }
+     return 'not-handled' // 不处理
+     // 如果不返回 则是回退到本机命令处理
+  }
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
+  }
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState))
+  }
+
   render() {
     return (
-      <Editor
-        onChange={this.onChange} 
-        editorState = {this.state.editorState} 
-      /> 
+      <div>
+        <button onClick={this.onUnderlineClick}>Underline</button>
+        <button onClick={this.onToggleCode}>Code</button>
+        <Editor
+          onChange={this.onChange} // 状态改变函数
+          editorState = {this.state.editorState} // 顶级状态
+
+          handleKeyCommand={this.handleKeyCommand} // 返回代表命令的字符串
+
+        /> 
+      </div>
     )
   }
 }
